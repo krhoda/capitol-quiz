84045b50b5175d04a99e1d955ec19a994e62651a
K <kelseydrhoda@gmail.com>
2019-11-28 19:25:17 -0700

    Silly proof of concept

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
 module Lib
-    ( someFunc
+    ( startNewGame
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- import qualified Data.Set as Set
+
+intro :: IO ()
+intro = putStrLn "Welcome to TBD GAME NAME, let's play a game!"
+
+startNewGame :: IO ()
+startNewGame = do
+  intro
+  askAll (createQuestions [("Colorado", "Denver"),("Wyoming", "Cheyanne"),("Arizona", "Phoenix")])
+
+createQuestions :: [Entry] -> [IO ()]
+createQuestions x = map askAStupidQuestion x
+
+askAStupidQuestion :: Entry -> IO ()
+askAStupidQuestion (reg, cap) = do
+  putStrLn ("What is the capitol of " ++ reg ++ "?")
+  x <- getLine
+  putStrLn ("I hope your answer \"" ++ x ++ "\" is " ++ cap ++ ", because " ++ cap ++ " is the right answer.")
+
+askAll :: [IO ()] -> IO ()
+askAll [] = putStrLn "done"
+askAll x = do
+  (head x)
+  askAll (tail x)
+
+type Capitol = String
+type Region = String
+type Entry = (Region, Capitol)
+
+  -- [("Colorado", "Denver"),("Wyoming", "Cheyanne"),("Arizona", "Phoenix")]
